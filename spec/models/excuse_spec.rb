c5ad47afcec9fbf1e28a57fe6ed688cb70d3772c
iamian1 <iansabolik@gmail.com>
2023-02-13 19:18:40 -0600

    Added RSpec for new validations

--- spec/models/excuse_spec.rb
+++ spec/models/excuse_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Excuse, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "file attachment validations" do
+    let(:excuse) { build(:excuse) }
+
+    context "file size is >= 5MB" do
+      before do
+        file = Rack::Test::UploadedFile.new(Rails.root.join("spec/fixtures/large_file.pdf"), "application/pdf")
+        excuse.file.attach(file)
+      end
+      it "is invalid" do
+        expect(excuse).to be_invalid
+        expect(excuse.errors[:file]).to include("File size is too large (Size > 5MB)")
+      end
+    end
+
+    context "file is not a PDF" do
+      before do
+        file = Rack::Test::UploadedFile.new(Rails.root.join("spec/fixtures/non_pdf.txt"), "text/plain")
+        excuse.file.attach(file)
+      end
+      it "is invalid" do
+        expect(excuse).to be_invalid
+        expect(excuse.errors[:file]).to include("File must be a PDF")
+      end
+    end
+  end
 end
