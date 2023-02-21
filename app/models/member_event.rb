64779c48f1161cc55ce72e5024faae97af1b7574
lacarbo05 <lacarbo@tamu.edu>
2023-02-21 07:42:41 -0600

    minor ui change to member_event

--- app/models/member_event.rb
+++ app/models/member_event.rb
@@ -3,4 +3,15 @@ class MemberEvent < ApplicationRecord
   belongs_to :event
 
   has_one_attached :file
+
+  before_save do
+    self.approved_status = false if approved_status.nil?
+    if (!!approved_status) then
+      self.approve_date = DateTime.now if approve_date.blank?
+    else
+      self.approve_date = nil
+    end
+  end
+
+
 end
