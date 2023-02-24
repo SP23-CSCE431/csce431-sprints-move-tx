1aeaf73c3bc564dc07e3b123e4abef9d48211e6e
lacarbo05 <lacarbo@tamu.edu>
2023-02-24 08:51:47 -0600

    minor updates before switching branches

--- app/models/member_event.rb
+++ app/models/member_event.rb
@@ -6,7 +6,7 @@ class MemberEvent < ApplicationRecord
 
   before_save do
     self.approved_status = false if approved_status.nil?
-    if (!!approved_status) then
+    if (approved_status == true) then
       self.approve_date = DateTime.now if approve_date.blank?
     else
       self.approve_date = nil
