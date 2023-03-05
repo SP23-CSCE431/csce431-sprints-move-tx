2f5163e1b551caff8f0dacfd598cdb37baa5b4f0
iamian1 <iansabolik@gmail.com>
2023-03-04 19:46:16 -0600

    Removed some redundant migrations that were causing issues.

--- db/migrate/20230301044429_add_member_to_committee.rb
+++
@@ -1,5 +0,0 @@
-class AddMemberToCommittee < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :committees, :member, foreign_key: true
-  end
-end
--- db/migrate/20230301044631_remove_leader_member_id_from_committees.rb
+++
@@ -1,5 +0,0 @@
-class RemoveLeaderMemberIdFromCommittees < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :committees, :leader_member_id, :integer
-  end
-end
