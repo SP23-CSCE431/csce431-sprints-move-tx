0ad65c8c344dd2b9705994cac05022cc95d579a9
lacarbo05 <lacarbo@tamu.edu>
2023-04-02 10:12:14 -0500

    update invalid attribute testing for necessary models

--- spec/unit/unit_member_spec.rb
+++ spec/unit/unit_member_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe Member, type: :model do
     # Test Inputs
     let!(:committee1) {
-      Committee.create!(
-          name: 'MyCommittee1'
-      )
-  }
+        Committee.create!(
+            name: 'MyCommittee1'
+        )
+    }
     let(:valid_attributes) {
         {
             name: 'MyName1',
@@ -19,52 +19,76 @@ RSpec.describe Member, type: :model do
             totalPoints: 40046
         }
     }
+    let(:invalid_attributes) {
+        {
+            name: '!nval!d',
+            position: '!nval!d'
+        }
+    }
 
     subject do
-      described_class.new(valid_attributes)
+        described_class.new(valid_attributes)
     end
     # It is valid with valid attributes
-    it 'is valid with valid attributes' do
-      expect(subject).to be_valid
+        it 'is valid with valid attributes' do
+        expect(subject).to be_valid
     end
+
     # Checks that it is not valid without a name
     it 'is not valid without a name' do
-      subject.name = nil
-      expect(subject).not_to be_valid
+        subject.name = nil
+        expect(subject).not_to be_valid
     end
+    # Checks that it is not valid with an invalid name
+    it 'is not valid with invalid name' do
+        subject.name = invalid_attributes[:name]
+        expect(subject).not_to be_valid
+    end
+
     # Checks that it is valid without a committee
     it 'is valid without a committee' do
-      subject.committee = nil
-      expect(subject).to be_valid
+        subject.committee = nil
+        expect(subject).to be_valid
     end
+
     # Checks that it is valid without a position
     it 'is valid without a position' do
-      subject.position = nil
-      expect(subject).to be_valid
+        subject.position = nil
+        expect(subject).to be_valid
     end
+    # Checks that it is not valid with an invalid name
+    it 'is not valid with invalid position' do
+        subject.position = invalid_attributes[:position]
+        expect(subject).not_to be_valid
+    end
+
     # Checks that it is valid without a civic points
     it 'is valid without civic points' do
-      subject.civicPoints = nil
-      expect(subject).to be_valid
+        subject.civicPoints = nil
+        expect(subject).to be_valid
     end
+
     # Checks that it is valid without a outreach points
     it 'is valid without outreach points' do
-      subject.outreachPoints = nil
-      expect(subject).to be_valid
+        subject.outreachPoints = nil
+        expect(subject).to be_valid
     end
+
     # Checks that it is valid without a social points
     it 'is valid without social points' do
-      subject.socialPoints = nil
-      expect(subject).to be_valid
+        subject.socialPoints = nil
+        expect(subject).to be_valid
     end
+
     # Checks that it is valid without a marketing points
     it 'is valid without marketing poitns' do
-      subject.marketingPoints = nil
-      expect(subject).to be_valid
+        subject.marketingPoints = nil
+        expect(subject).to be_valid
     end
+
     # Checks that it is valid without a total points
     it 'is valid without total points' do
-      subject.totalPoints = nil
-      expect(subject).to be_valid
+        subject.totalPoints = nil
+        expect(subject).to be_valid
     end
-  end
+end
