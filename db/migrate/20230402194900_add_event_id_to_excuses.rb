2fb211e0459d9b315c88ce20fee19caadb3ab974
iamian1 <iansabolik@gmail.com>
2023-04-04 13:41:29 -0500

    Made member and event not required for excuses

--- db/migrate/20230402194900_add_event_id_to_excuses.rb
+++ db/migrate/20230402194900_add_event_id_to_excuses.rb
@@ -1,5 +1,5 @@
 class AddEventIdToExcuses < ActiveRecord::Migration[7.0]
   def change
-    add_reference :excuses, :event, null: false, foreign_key: true
+    add_reference :excuses, :event, foreign_key: true
   end
 end
